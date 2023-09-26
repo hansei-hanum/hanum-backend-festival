b9dbfb1cfe5870e096a343dbec2b9ee53fcbbb75
plkmnb100 <pcy06@kakao.com>
2023-09-26 18:58:58 +0900

    feat: get my number, time-bound

--- env/environ.py
+++ env/environ.py
@@ -8,6 +8,7 @@ class Env:
     HOST = os.environ.get("HOST", "0.0.0.0")
     PORT = int(os.environ.get("PORT", 80))
     DEBUG = os.environ.get("DEBUG", "False") == "True"
+    NO_LUCKY_NUMBER_UNTIL = os.environ.get("NO_LUCKY_NUMBER_UNTIL")
 
 
 class MicroEnv:
--- routes/luckydraw/get_number.py
+++ routes/luckydraw/get_number.py
@@ -4,6 +4,10 @@ from pydantic import BaseModel
 from micro import auth
 from database import scope, LuckyNumber, LuckyToken
 from sqlalchemy import select
+from env import Env
+from datetime import datetime, timezone
+from pytz import timezone
+
 
 router = APIRouter()
 
@@ -14,6 +18,12 @@ class GetLuckyNumberRequest(BaseModel):
 
 @router.post("/lucky_numbers")
 async def get_lucky_number(request: GetLuckyNumberRequest, userid=Depends(RequireAuth)):
+    if Env.NO_LUCKY_NUMBER_UNTIL:
+        no_lucky_number_until = datetime.fromisoformat(Env.NO_LUCKY_NUMBER_UNTIL)
+
+        if datetime.now(timezone("Asia/Seoul")) < no_lucky_number_until:
+            raise HTTPException(403, "NOT_STARTED_YET")
+
     user = await auth.get_user(userid)
 
     if not user:
@@ -47,8 +57,31 @@ async def get_lucky_number(request: GetLuckyNumberRequest, userid=Depends(Requir
         token.user_id = userid
 
         lucky_number = LuckyNumber(user_id=userid)
-        await session.add(lucky_number)
+        session.add(lucky_number)
         await session.commit()
         await session.refresh(lucky_number)
+
+        return {"message": "SUCCESS", "data": lucky_number.id}
+
+
+@router.get("/lucky_number")
+async def get_lucky_number(userid=Depends(RequireAuth)):
+    user = await auth.get_user(userid)
+
+    if not user:
+        raise HTTPException(404, "USER_NOT_FOUND")
+
+    if not user.verification.type in ["STUDENT", "TEACHER"]:
+        raise HTTPException(403, "NOT_ALLOWED")
+
+    async with scope() as session:
+        lucky_number = (
+            await session.execute(
+                select(LuckyNumber).where(LuckyNumber.user_id == userid)
+            )
+        ).scalar_one_or_none()
+
+        if not lucky_number:
+            raise HTTPException(404, "USER_HAS_NO_LUCKY_NUMBER")
 
         return {"message": "SUCCESS", "data": lucky_number.id}
