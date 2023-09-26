27ea85a33a65f41a1d581937fe08f1ced29425fc
plkmnb100 <pcy06@kakao.com>
2023-09-26 20:14:32 +0900

    fix: wrong requirements

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
+gunicorn main:app --workers 8 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
