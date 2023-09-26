f403e5180db652d6f75b291f6919b6803a99fb08
최근원 <kidjustinchoi@gmail.com>
2023-09-26 13:34:56 +0900

    feat: add luckydraw route

--- neis/__init__.py
+++
@@ -1,3 +0,0 @@
-from .meal import MealAPI
-from .timetable import TimeTable
-from .schedule import Schedule
---
+++ routes/luckydraw/__init__.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter
+
+router = APIRouter("/luckydraw", tags=["luckydraw"])
